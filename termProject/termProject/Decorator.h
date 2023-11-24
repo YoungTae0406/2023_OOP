6f9e91a0d9cfe15f32edb2e803d04dac00c4be25
YoungTae0406 <43427176+YoungTae0406@users.noreply.github.com>
2023-11-24 22:36:39 +0900

    feat : Rebuilding

---
+++ termProject/termProject/Decorator.h
@@ -0,0 +1,111 @@
+#pragma once
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+class MyString {
+public:
+	int x; int y;
+	std::string word;
+	int speed;
+	bool isCorrect;
+	MyString();
+	MyString(std::string& w);
+};
+
+MyString::MyString() {}
+MyString::MyString(std::string& w) : word(w) {
+	this->x = 0; this->y = 0;
+	this->speed = 1; this->isCorrect = false;
+}
+
+
+class RandomStringComponent {
+public:
+	virtual std::string getRandomString() = 0;
+	virtual ~RandomStringComponent();
+};
+
+class BasicString : public RandomStringComponent {
+public:
+	std::string getRandomString() override;
+};
+
+class StringDecorator : public RandomStringComponent {
+protected:
+	RandomStringComponent* RSC;
+public:
+	StringDecorator(RandomStringComponent* rsc);
+	std::string getRandomString() override;
+	virtual ~StringDecorator();
+};
+
+class AlphabetDecorator : public StringDecorator {
+public:
+	AlphabetDecorator(RandomStringComponent* rsc);
+	std::string getRandomString() override;
+};
+
+class NumberDecorator : public StringDecorator {
+public:
+	NumberDecorator(RandomStringComponent* rsc);
+	std::string getRandomString() override;
+};
+
+class SpecialDecorator : public StringDecorator {
+public:
+	SpecialDecorator(RandomStringComponent* rsc);
+	std::string getRandomString() override;
+};
+
+RandomStringComponent::~RandomStringComponent() {}
+
+std::string BasicString::getRandomString() {
+	return "aaaaabbbbb";
+}
+
+StringDecorator::StringDecorator(RandomStringComponent* rsc) : RSC(rsc) {}
+
+std::string StringDecorator::getRandomString() {
+	return RSC->getRandomString();
+}
+
+StringDecorator::~StringDecorator() {
+	delete RSC;
+}
+
+AlphabetDecorator::AlphabetDecorator(RandomStringComponent* rsc) : StringDecorator(rsc) {}
+
+std::string AlphabetDecorator::getRandomString() {
+	std::string charset = "abcdefghijklmnopqrstuvwxyz";
+	std::string result = StringDecorator::getRandomString();
+	for (int i = 0; i < result.size(); i++) {
+		int index = rand() % charset.size();
+		result[i] = charset[index];
+	}
+	return result;
+}
+
+NumberDecorator::NumberDecorator(RandomStringComponent* rsc) : StringDecorator(rsc) {}
+
+std::string NumberDecorator::getRandomString() {
+	std::string charset = "abcdefghijklmnopqrstuvwxyz0123456789";
+	std::string result = StringDecorator::getRandomString();
+	for (int i = 0; i < result.size(); i++) {
+		int index = rand() % charset.size();
+		result[i] = charset[index];
+	}
+	return result;
+}
+
+SpecialDecorator::SpecialDecorator(RandomStringComponent* rsc) : StringDecorator(rsc) {}
+
+std::string SpecialDecorator::getRandomString() {
+	std::string charset = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
+	std::string result = StringDecorator::getRandomString();
+	for (int i = 0; i < result.size(); i++) {
+		int index = rand() % charset.size();
+		result[i] = charset[index];
+	}
+	return result;
+}
--- termProject/termProject/main.cpp
+++ termProject/termProject/main.cpp
@@ -8,177 +8,107 @@
 #include <conio.h>
 #include <vector>
 #include <iomanip>
-#include <mutex>
+#include "Cursor.h"
+#include "Decorator.h"
 
-const int WIDTH = 80;
-const int HEIGHT = 15;
-bool isGameRunning = true;
-static int thr_exit = 0;
-std::thread inputThread;
-std::string inputBuffer;
-std::mutex mtx;
+void printString(int i);
+void eraseString(int i);
+void input(int c);
+void strCompare();
 
-class MyString {
-public:
-	int x;
-	std::string word;
-	int speed;
-	MyString() {}
+std::string userInput;
+std::vector<MyString> v;
+int MAXWIDTH = 20;
+int END = 0;
 
-	MyString(std::string& w) : word(w) { this->x = 0; this->speed = 1; }
-
-	void setX(int p) { // x축 위치 설정
-		this->x = p;
-	}
-	void setSpeed(int p) {
-		this->speed = p;
-	}
-
-};
-
-class RandomStringComponent {
-public:
-	virtual std::string getRandomString() = 0;
-	virtual ~RandomStringComponent() {
-
-	}
-};
-
-class BasicString : public RandomStringComponent {
-public:
-	std::string getRandomString() override {
-		std::string a;
-		a = "aaaaabbbbb";
-		return a;
-	}
-
-};
-
-class StringDecorator : public RandomStringComponent {
-protected:
-	RandomStringComponent* RSC;
-public:
-	StringDecorator(RandomStringComponent* rsc) : RSC(rsc) {}
-	std::string getRandomString() override {
-		return RSC->getRandomString();
-	}
-	virtual ~StringDecorator() {
-		delete RSC;
-	}
-
-};
-
-class AlphabetDecorator : public StringDecorator {
-public:
-	AlphabetDecorator(RandomStringComponent* rsc) : StringDecorator(rsc) {}
-	std::string getRandomString() override {
-
-		std::string charset = "abcdefghijklmnopqrstuvwxyz";
-		std::string result = StringDecorator::getRandomString();
-		for (int i = 0; i < 10; i++) {
-			int index = rand() % charset.size();
-			result[i] = charset[index];
-		}
-
-		return result;
-	}
-
-};
-
-class NumberDecorator : public StringDecorator {
-public:
-	NumberDecorator(RandomStringComponent* rsc) : StringDecorator(rsc) {}
-	std::string getRandomString() override {
-
-		std::string charset = "abcdefghijklmnopqrstuvwxyz0123456789";
-		std::string result = StringDecorator::getRandomString();
-		for (int i = 0; i < 10; i++) {
-			int index = rand() % charset.size();
-			result[i] = charset[index];
-		}
-
-		return result;
-	}
-
-};
-class SpecialDecorator : public StringDecorator {
-public:
-	SpecialDecorator(RandomStringComponent* rsc) : StringDecorator(rsc) {}
-	std::string getRandomString() override {
-		
-		std::string charset = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*";
-		std::string result = StringDecorator::getRandomString();
-		for (int i = 0; i < 10; i++) {
-			int index = rand() % charset.size();
-			result[i] = charset[index];
-		}
-
-		return result;
-	}
-};
-
-void printScreen(std::vector<MyString>& v) {
-
-	system("cls");
-
-	for (int i = 0; i < v.size(); ++i) {
-		std::cout << std::setw(v[i].x) << "" << v[i].word << std::endl;
-		
-	}
-	std::cout << "-------------------------------------\n";
-	std::cout << "입력 : " << inputBuffer << std::flush;
+void printString(int i) {
+	Cursor::gotoxy(v[i].x, v[i].y);
+	std::cout << v[i].word;
 }
 
-void downWord(std::vector<MyString>& v) {
-	for (int i = v.size()-1; i > 0; i--) {
-		v[i].word = v[i - 1].word;
-		v[i].x = v[i - 1].x;
-		v[i - 1].x = 0;
-		v[i - 1].word = " ";
-
+void eraseString(int i) {
+	Cursor::gotoxy(v[i].x, v[i].y);
+	for (int i = 0; i < MAXWIDTH-1; i++) {
+		std::cout << " ";
 		
 	}
-	
 }
 
-void processInput() {
-	while (!thr_exit) {
-		
-		char ch = getchar();
-		if (ch == '\n') {
-			inputBuffer.clear();
+void input(int c) {
+	if (c == '\b') { // 백스페이스 누를때
+		if (userInput.length() != 0) {
+			Cursor::gotoxy(6 + userInput.length() - 1, 23);
+			std::cout << " ";
+			userInput.pop_back();
+		}
+	}
+	else if (c != '\r') { // 엔터키 제외한 다른 키
+		Cursor::gotoxy(6 + userInput.length(), 23);
+		printf("%c", c);
+		userInput.push_back(c);
+	}
+	else if (c == '\r') { // 엔터 키
+		strCompare();
+		Cursor::gotoxy(6, 23);
+		for (int i = 0; i < userInput.length(); i++)
+			std::cout << " ";
+		userInput.clear();
+		//ending();
+	}
+}
+void strCompare() {
+	for (int i = 0; i < v.size(); i++) {
+		if (userInput.compare(v[i].word) == 0) { // 문자열이 같으면
+			v[i].isCorrect = true;
+			eraseString(i);
+			v[i].word = "";
+		}
+	}
+}
+void setRandomX() {
+	for (int i = 0; i < v.size(); i++) {
+		int temp = rand() % 4 + 1;
+		if (temp == 1) {
+			int rx = MAXWIDTH/2;
+			v[i].x = rx;
+		}
+		else if (temp == 2) {
+			int rx = MAXWIDTH + MAXWIDTH/2;
+			v[i].x = rx;
+		}
+		else if (temp == 3) {
+			int rx = 2*MAXWIDTH + MAXWIDTH/2;
+			v[i].x = rx;
 		}
 		else {
-			inputBuffer += ch;
+			int rx = 3*MAXWIDTH + MAXWIDTH/2;
+			v[i].x = rx;
 		}
-
-
 	}
 }
-void start_thread() {
-	thr_exit = 0;
-	inputThread = std::thread(processInput);
-	inputThread.detach();
-}
-void end_thread() {
-	thr_exit = 1;
-	if (inputThread.joinable()) {
-		inputThread.join();
-	}
-	
-}
+void drawBorder() {
+	Cursor::gotoxy(0, 21);
+	std::cout << "---------------------------------------------------------------------------------------------\n\n";
+	std::cout << "입력: ";
 
-std::vector<MyString> v;
+}
+void countEnd() {
+	END++;
+}
 
 int main() {
+	//Cursor::hideCursor();
+	//std::vector<MyString> v;
 	srand(static_cast<unsigned int>(time(0)));
     int choice;
-    RandomStringComponent* generator = new BasicString();
+    RandomStringComponent* generator = new BasicString(); // Decorator.h
 	
     // 내 컴퓨터 콘솔창 크기. 세로 30, 가로 120
-	// 5등분 각각 24칸을 차지.
+	// 4등분 각각 30칸을 차지.
     
     std::cout << "타자게임을 시작합니다." << std::endl;
+	std::cout << "총 20개의 단어가 나옵니다." << std::endl;
+	std::cout << "20개가 나오고 게임이 종료되면 점수가 나옵니다." << std::endl;
     std::cout << "옵션을 선택해주세요 \n1. Alphabet\n2. Number\n3. SpecialChar\n" << std::endl;
     std::cin >> choice;
 
@@ -196,28 +126,50 @@ int main() {
         std::cout << "1, 2, 3 을 입력하여 옵션을 선택해야합니다.\n";
         return 1;
     }
-    
+
 	for (int i = 0; i < 20; i++) {
 		std::string temp = generator->getRandomString();
 		v.push_back(MyString(temp));
 	}
+	setRandomX();
+
+	system("cls"); 
+
+	int c;
+	clock_t time1 = clock();
 	
-	//std::thread screenThread(printScreen, std::ref(v));
-	start_thread();
+	int count = 1;
 	
-	while (true) {
-		Sleep(1000);
-		downWord(v);
-		printScreen(v);
+	while (1) {
+		if (_kbhit()) {
+			c = _getch();
+			input(c);
+		}
+		if (clock() - time1 > 1000) {
+			for (int i = 0; i < count; i++) {
+				drawBorder();
+				eraseString(i);
+				v[i].y++;
+				if (v[i].y == 21) {
+					countEnd();
+					v[i].word = "";
+					eraseString(i);
+				}
+				else {
+					printString(i);
+					
+				}
+			}
+			if (count < 20) {
+				count++;
+			}
+			time1 = clock();
+		}
+		if (END==20) {
+			break;
+		}
+		
 	}
-
-
-	//screenThread.join();
-	end_thread();
-	
-	
-	
-	
 
     return 0;
 }
